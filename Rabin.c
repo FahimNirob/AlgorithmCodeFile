d9f380661d06c5b4aa4dc3e5a4069ef60d4ee3d5
Asibul Hasan <asibulhasan.rucse.18@gmail.com>
2020-01-12 23:20:36 +0600

    Rabin karp completed

--- Rabin.c
+++ Rabin.c
@@ -3,7 +3,7 @@
 
 #define d 256
 
-void sFunc(char pat[], char txt[], int q)
+void Rabin(char pat[], char txt[], int q)
 {
     int patternLen = strlen(pat);
     int txtLen = strlen(txt);
@@ -12,8 +12,9 @@ void sFunc(char pat[], char txt[], int q)
     int tHash = 0; 
     int h = 1;
     for (i = 0; i < patternLen-1; i++)
-        h = (h*d)%q;
-
+        {
+            h = (h*d)%q;
+        }
     // Calculate the hash value of pattern and first window of text
     for (i = 0; i < patternLen; i++)
     {
@@ -21,37 +22,36 @@ void sFunc(char pat[], char txt[], int q)
         tHash = (d*tHash + txt[i])%q;
     }
 
-    // Slide the pattern over text one by one
+    // Sliding the pattern over text one by one
     for (i = 0; i <= txtLen - patternLen; i++)
     {
-
-        // Check the hash values of current window of text
-        // and pattern. If the hash values match then only
-        // check for characters on by one
+        /*Check the hash values of current window of text
+        and pattern. If the hash values match then only
+        check for characters on by one*/
         if ( pHash == tHash )
         {
             /* Check for characters one by one */
             for (j = 0; j < patternLen; j++)
             {
                 if (txt[i+j] != pat[j])
-                    break;
+                    {
+                        break;
+                    }
             }
-
             // if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
             if (j == patternLen)
-                printf("Pattern found at index %d \n", i);
+                {
+                    printf("Pattern found at index %d \n", i);
+                }
         }
-
-        // Calculate hash value for next window of text: Remove
-        // leading digit, add trailing digit
+        /*Calculate hash value for next window of text*/
         if ( i < txtLen- patternLen )
         {
             tHash = (d*(tHash - txt[i]*h) + txt[i+patternLen])%q;
-
-            // We might get negative value of t, converting it
-            // to positive
             if (tHash < 0)
-            tHash = (tHash + q);
+            {
+                tHash = (tHash + q);
+            }
         }
     }
 }
@@ -66,6 +66,6 @@ int main()
     scanf("%s",&pat);
     printf("Enter a prime number \n");
     scanf("%d",&q);
-    sFunc(pat, txt, q);
+    Rabin(pat, txt, q);
     return 0;
 }
